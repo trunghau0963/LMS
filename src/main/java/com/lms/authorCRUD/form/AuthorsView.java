5dce243e475719a1b40a13e2927b55c3081eda22
Nguyễn Trần Trung Hậu <96047179+trunghau0963@users.noreply.github.com>
2024-05-02 16:12:58 +0700

    Refactor AuthorDao, AuthorRepo, and AuthorService to use gender as a boolean instead of a string

--- src/main/java/com/lms/authorCRUD/form/AuthorsView.java
+++ src/main/java/com/lms/authorCRUD/form/AuthorsView.java
@@ -1,26 +1,28 @@
 package com.lms.authorCRUD.form;
 
-import javax.swing.*;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
 
-// import com.lms.authorCRUD.form.other.AddAuthorPanel;
+import com.lms.authorCRUD.repo.AuthorRepo;
+import com.lms.authorCRUD.dal.AuthorDao;
 import com.lms.authorCRUD.form.other.ListAuthorPanel;
-import com.lms.authorCRUD.form.other.temp.EditInfoAuthorPanel;
-
-import java.awt.*;
+import com.lms.authorCRUD.service.AuthorService;
 
 public class AuthorsView extends JPanel {
     private CardLayout cardLayout;
     ListAuthorPanel authors;
-    // EditInfoAuthorPanel editAuthor;
+    private AuthorService authorService;
+    private AuthorDao authorDao;
 
     public AuthorsView() {
         // Tạo CardLayout
         super();
-
         cardLayout = new CardLayout();
         this.setLayout(cardLayout);
 
-        authors = new ListAuthorPanel(cardLayout, this);
+        authorDao = AuthorRepo.getInstance();
+        authorService = new AuthorService(authorDao);
+        authors = new ListAuthorPanel(authorService);
         
         // Panel trống ban đầu
         this.add(authors, "authorsPage");
